beaf49e85dab944a219e001d1bb5edee44275e45
RogueStar112 <hildtgunnt@gmail.com>
2024-05-19 07:58:53 +0100

    Added rollback prefix to createuserstable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,6 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+        DB::rollBack();
+
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
