7b728f2d60ca13ca538b7f98b7da44ff13d16ca0
RogueStar112 <hildtgunnt@gmail.com>
2024-05-19 11:54:48 +0100

    Reverted indexing

--- resources/views/mybudget/mybudget_createcategory.blade.php
+++ resources/views/mybudget/mybudget_createcategory.blade.php
@@ -1050,7 +1050,7 @@
         //document.getElementById('category-name-1-edit').setAttribute("value", "text");
 
         // Subtract id by 1 since JSON indexing starts at 0
-        var category = @json($categories)[id-2];
+        var category = @json($categories)[id-1];
         
         // Prepare THE FORM to edit the appropriate category.
 
