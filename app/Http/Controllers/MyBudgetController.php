76fe764fd3a0e6e0a7beaead8ebf76da84971347
RogueStar112 <hildtgunnt@gmail.com>
2024-05-19 18:31:43 +0100

    Delete_form fix 3

--- app/Http/Controllers/MyBudgetController.php
+++ app/Http/Controllers/MyBudgetController.php
@@ -502,6 +502,7 @@ class MyBudgetController extends Controller
                                 ->select('mybudget_item.*', 'mybudget_category.name as category_name', 'mybudget_section.name as section_name', 'mybudget_source.name as source_name')
                                 //->selectRaw('PRINTF("%.2f", mybudget_item.price) as price_twodp')
                                 // ->selectRaw("REPLACE(mybudget_item.price, ',', '') as price_twodp")
+                                ->select('mybudget_item.name as name')
                                 ->select('mybudget_item.price as price_twodp')
                                 ->where("mybudget_item.id", "=", "$id")
                                 ->where('mybudget_item.user_id', "=", "$insert_userid")
