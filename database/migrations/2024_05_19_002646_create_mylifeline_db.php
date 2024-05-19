8a25b7e8ab3a794d4a7e0d12cb1ea49ef429644f
RogueStar112 <97138925+RogueStar112@users.noreply.github.com>
2024-05-19 02:26:32 +0100

    fixed migration file

--- database/migrations/2024_05_19_002646_create_mylifeline_db.php
+++ database/migrations/2024_05_19_002646_create_mylifeline_db.php
@@ -13,169 +13,69 @@ class CreateMylifelineDb extends Migration
      */
     public function up()
     {
+        // Create mybudget_item table
         Schema::create('mybudget_item', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 30)->default('');
+            $table->decimal('price', 8, 2)->default(0.00);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        // Create mybudget_source table
+        Schema::create('mybudget_source', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30)->default('');
             $table->timestamps();
         });
 
-        Schema::table('mybudget_item', function (Blueprint $table) {
-            $table->string('name', 30);
-            $table->decimal('price', $precision = 8, $scale = 2);
-            $table->date('created_at');
-        });
-
-        Schema::create('mybudget_source', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 30);
-            $table->date('created_at');
-        });
-
-        // Schema::table('users', function (Blueprint $table) {
-        //     $table->integer('source_id');
-            
-        //     $table->foreign('source_id')->references('id')->on('mybudget_item');
-
-
-        // });
-
-        Schema::create('mybudget_category', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 30);
-            $table->decimal('budget', $precision = 8, $scale = 2);
-            $table->decimal('cost', $precision = 8, $scale = 2);
-            $table->date('created_at');
-        });
-
-        Schema::create('mybudget_section', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 30);
-            $table->decimal('budget', $precision = 8, $scale = 2);
-            $table->decimal('cost', $precision = 8, $scale = 2);
-            $table->date('created_at');
-        });
-
-        Schema::table('mybudget_section', function (Blueprint $table) {
-            $table->integer('category_id');
-            
-            $table->foreign('category_id')->references('id')->on('mybudget_category');
-        });
-
-        Schema::create('mybudget_budget', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 30);
-            $table->decimal('budget', $precision = 8, $scale = 2);
-            $table->decimal('cost', $precision = 8, $scale = 2);
-            $table->decimal('balance-spent', $precision = 8, $scale = 2);
-            $table->decimal('balance-left', $precision = 8, $scale = 2);
-
-            $table->date('created_at');
-        });
-
-        Schema::table('mybudget_item', function (Blueprint $table) {
-            $table->integer('category_id');
-            $table->foreign('category_id')->references('id')->on('mybudget_category');
-
-            $table->integer('section_id');
-            $table->foreign('section_id')->references('id')->on('mybudget_section');
-            
-            $table->integer('source_id');
-            $table->foreign('source_id')->references('id')->on('mybudget_source');
-        });
-        
-        Schema::table('mybudget_item', function (Blueprint $table) {
-            $table->string('name', 30)->default('');
-            $table->decimal('price', $precision = 8, $scale = 2)->default(0.00);
-            $table->date('created_at');
-        });
-
-        Schema::table('mybudget_item', function (Blueprint $table) {
-            $table->softDeletes($column = 'deleted_at', $precision = 0);
-        });
-
-        Schema::create('mybudget_source', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 30)->default('');
-            $table->date('created_at');
-        });
-
-        // Schema::table('users', function (Blueprint $table) {
-        //     $table->integer('source_id');
-            
-        //     $table->foreign('source_id')->references('id')->on('mybudget_item');
-
-
-        // });
-        
-
+        // Create mybudget_category table
         Schema::create('mybudget_category', function (Blueprint $table) {
             $table->id();
             $table->string('name', 30)->default('');
-            //$table->decimal('budget', $precision = 8, $scale = 2)->default(0.00);
-            //$table->decimal('cost', $precision = 8, $scale = 2)->default(0.00);
-            $table->date('created_at');
+            $table->timestamps();
         });
 
+        // Create mybudget_section table
         Schema::create('mybudget_section', function (Blueprint $table) {
             $table->id();
             $table->string('name', 30)->default('');
-            //$table->decimal('budget', $precision = 8, $scale = 2)->default(0.00);
-            //table->decimal('cost', $precision = 8, $scale = 2)->default(0.00);
-            $table->date('created_at');
+            $table->timestamps();
+            $table->foreignId('category_id')->constrained('mybudget_category')->onDelete('cascade');
         });
 
-        
-
-        Schema::table('mybudget_section', function (Blueprint $table) {
-            //$table->integer('category_id')->default(0);
-            
-            $table->foreign('category_id')->references('id')->on('mybudget_category')->onDelete('cascade');;
-        });
-        
-
+        // Create mybudget_budget table
         Schema::create('mybudget_budget', function (Blueprint $table) {
             $table->id();
             $table->string('name', 30)->default('');
-            $table->decimal('budget', $precision = 8, $scale = 2)->default(0.00);
-            $table->decimal('cost', $precision = 8, $scale = 2)->default(0.00);
-            $table->decimal('balance-spent', $precision = 8, $scale = 2)->default(0.00);
-            $table->decimal('balance-left', $precision = 8, $scale = 2)->default(0.00);
-
-            $table->date('created_at');
+            $table->decimal('budget', 8, 2)->default(0.00);
+            $table->decimal('cost', 8, 2)->default(0.00);
+            $table->decimal('balance_spent', 8, 2)->default(0.00);
+            $table->decimal('balance_left', 8, 2)->default(0.00);
+            $table->timestamps();
         });
 
-
+        // Add foreign keys to mybudget_item table
         Schema::table('mybudget_item', function (Blueprint $table) {
-            //$table->integer('category_id')->default(0);
-            $table->foreign('category_id')->references('id')->on('mybudget_category')->onDelete('cascade');;
-
-            //$table->integer('section_id')->default(0);
-            $table->foreign('section_id')->references('id')->on('mybudget_section')->onDelete('cascade');;
-            
-            //$table->integer('source_id')->default(0);
-            $table->foreign('source_id')->references('id')->on('mybudget_source')->onDelete('cascade');;
+            $table->foreignId('category_id')->nullable()->constrained('mybudget_category')->onDelete('cascade');
+            $table->foreignId('section_id')->nullable()->constrained('mybudget_section')->onDelete('cascade');
+            $table->foreignId('source_id')->nullable()->constrained('mybudget_source')->onDelete('cascade');
         });
 
+        // Create mybudget_subtransactions table
         Schema::create('mybudget_subtransactions', function (Blueprint $table) {
-
             $table->id();
             $table->timestamps();
-            $table->decimal('price', $precision = 8, $scale = 2);
-            $table->string('description', 300);
-            $table->integer('category_id');
-            $table->integer('section_id')->nullable(false)->default('');
-            $table->integer('transaction_id')->nullable(false)->default('');
-            $table->integer('source_id')->nullable(false)->default('');
-    
-        });
-
-        Schema::table('mybudget_subtransactions', function (Blueprint $table) {
+            $table->decimal('price', 8, 2)->default(0.00);
+            $table->string('description', 300)->default('');
             $table->string('name', 100)->default('');
-            $table->foreign('category_id')->references('id')->on('mybudget_category')->onDelete('cascade');
-            $table->foreign('section_id')->references('id')->on('mybudget_section')->onDelete('cascade');
-            $table->foreign('transaction_id')->references('id')->on('mybudget_item')->onDelete('cascade');
-            $table->foreign('source_id')->references('id')->on('mybudget_source')->onDelete('cascade');
+            $table->foreignId('category_id')->nullable()->constrained('mybudget_category')->onDelete('cascade');
+            $table->foreignId('section_id')->nullable()->constrained('mybudget_section')->onDelete('cascade');
+            $table->foreignId('transaction_id')->nullable()->constrained('mybudget_item')->onDelete('cascade');
+            $table->foreignId('source_id')->nullable()->constrained('mybudget_source')->onDelete('cascade');
         });
     }
+
     /**
      * Reverse the migrations.
      *
@@ -183,6 +83,11 @@ class CreateMylifelineDb extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('mylifeline_db');
+        Schema::dropIfExists('mybudget_subtransactions');
+        Schema::dropIfExists('mybudget_item');
+        Schema::dropIfExists('mybudget_source');
+        Schema::dropIfExists('mybudget_category');
+        Schema::dropIfExists('mybudget_section');
+        Schema::dropIfExists('mybudget_budget');
     }
 }
