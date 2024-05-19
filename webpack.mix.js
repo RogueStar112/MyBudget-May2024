9266523a68e383b26a23f2ad25ef21a349ec6209
RogueStar112 <hildtgunnt@gmail.com>
2024-05-19 08:37:28 +0100

    Mix fix attempt

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,10 +15,8 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
-    ]);
-
-mix.js('resources/js/app.js', 'public/js')
-.sass('resources/sass/app.scss', 'public/css')
+        require('autoprefixer'),
+    ])
 .styles([
 'public/css/app.css',
 'public/css/mybudget_setbudget.css',
@@ -33,8 +31,26 @@ mix.js('resources/js/app.js', 'public/js')
 'public/css/mylifeline_report.css',
 'public/css/mylifeline_section.css',
 'public/css/mylifeline_source.css'
-])
-;
+]);
+
+// mix.js('resources/js/app.js', 'public/js')
+// .sass('resources/sass/app.scss', 'public/css')
+// .styles([
+// 'public/css/app.css',
+// 'public/css/mybudget_setbudget.css',
+// 'public/css/mybudget_transactions_MOBILE.css',
+// 'public/css/mybudget_transactions.css',
+// 'public/css/mybudget_viewhistory_tooltips_for_menu.css',
+// 'public/css/mybudget_viewhistory.css',
+// 'public/css/myjournal_home.css',
+// 'public/css/mylifeline_comparison.css',
+// 'public/css/mylifeline_home.css',
+// 'public/css/mylifeline_itemhistory.css',
+// 'public/css/mylifeline_report.css',
+// 'public/css/mylifeline_section.css',
+// 'public/css/mylifeline_source.css'
+// ])
+// ;
 
 mix.autoload({
 jquery: ['$', 'window.jQuery', 'jQuery']
