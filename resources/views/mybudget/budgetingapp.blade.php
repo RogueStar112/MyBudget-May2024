a1d484d4f2a05747b6bd2aec2b719199f0f1b972
RogueStar112 <hildtgunnt@gmail.com>
2024-05-20 03:01:49 +0100

    Revert logout button change with comment until sorted

--- resources/views/mybudget/budgetingapp.blade.php
+++ resources/views/mybudget/budgetingapp.blade.php
@@ -33,12 +33,12 @@
                 <x-navbar-item url="/login" title="LOGIN" color="lightblue" icon="user-alt"/>
                 <x-navbar-item url="/register" title="REGISTER" color="skyblue" icon="user-plus"/>
                 <x-navbar-item url="/settings" title="SETTINGS" color="grey" icon="cog"/>
-                {{-- <x-navbar-item url="/logout" title="LOGOUT" color="red" icon="sign-out-alt" /> --}}
-                <form method="POST" action="{{route('logout')}}" style="background-color: red;">
-                    <input class="nav-link" type="submit">
-                    <i class="fas fa-logout">
+                <x-navbar-item url="/logout" title="LOGOUT" color="red" icon="sign-out-alt" />
+                {{-- <form method="POST" action="{{route('logout')}}" style="background-color: red;">
+                    <input class="nav-link" type="submit" />
+                    <i class="fas fa-logout" />
                     <div class="label-bottom">LOGOUT</div>
-                </form>
+                </form> --}}
                 @isset($userName)
 
                 <div class="diagonal-divider"></div>
