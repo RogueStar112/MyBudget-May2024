5ed6643bfe17e6ea4f98dafa2269a70fa3ffd9d9
RogueStar112 <hildtgunnt@gmail.com>
2024-05-21 13:24:59 +0100

    Welcome blade change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,9 +17,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <link href="{{ asset('css/mylifeline_home.css') }}" rel="stylesheet" type="text/css">
-
-    
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 </head>
 <body>
     <div id="app">
@@ -37,9 +34,24 @@
             <x-navbar-item url="/login" title="LOGIN" color="lightblue" icon="user-alt"/>
             <x-navbar-item url="/register" title="REGISTER" color="skyblue" icon="user-plus"/>
             <x-navbar-item url="/settings" title="SETTINGS" color="grey" icon="cog"/>
-
+ 
         </x-slot>
     </x-navbar>
+
+
+        <div class="text-center text-black">
+            <h2>Welcome to MyLifeline</h2>
+            <p>A series of practical daily-life apps.</p>
+
+            <ul>
+                <li class="text-green-600">MyBudget - For personal finance</li>
+                <li>MyNutrition - For physical health</li>
+                <li class="text-red-600">MyJournal - For mental journalling</li>
+                <li class="text-blue-600">MyReviews - To review anything, from food to places!</li>
+            </ul>
+        </div>
+
+    </div>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
