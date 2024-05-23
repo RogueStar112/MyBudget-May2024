0416e101f7f0dba022fb20cd81acbbcc9beb9042
RogueStar112 <97138925+RogueStar112@users.noreply.github.com>
2024-05-23 05:09:53 +0100

    fix navbar again 3

--- resources/views/components/navbar-item.blade.php
+++ resources/views/components/navbar-item.blade.php
@@ -1,6 +1,6 @@
 
 <li class="flex nav-item m-1 relative justify-around" style="background-color: {{ $color }};">
-    <a class="nav-link" aria-current="page" href="{{ $url }}"><i class="fas fa-{{ $icon }}"></i><p></p><div class="label-bottom">{{ $title }}</div>
+    <a class="nav-link flex relative justify-around" aria-current="page" href="{{ $url }}"><i class="fas fa-{{ $icon }}"></i><p></p><div class="label-bottom">{{ $title }}</div>
     
         <span class="visible md:collapse">{{ $title }}</span>
 
