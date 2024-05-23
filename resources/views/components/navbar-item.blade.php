7e96c5f90e19c9fecea3882176f4d93803f97c05
RogueStar112 <97138925+RogueStar112@users.noreply.github.com>
2024-05-23 05:27:23 +0100

    navbar mobile change

--- resources/views/components/navbar-item.blade.php
+++ resources/views/components/navbar-item.blade.php
@@ -1,8 +1,8 @@
 
-<li class="flex nav-item m-1 relative justify-around" style="background-color: {{ $color }};">
-    <a class="nav-link flex relative justify-around w-full" aria-current="page" href="{{ $url }}"><i class="fas fa-{{ $icon }}"></i><p></p><div class="collapse md:visible label-bottom">{{ $title }}</div>
+<li class="flex nav-item m-1 relative justify-around" >
+    <a class="nav-link flex relative justify-around w-full" aria-current="page" href="{{ $url }}"><i class="fas fa-{{ $icon }} p-4 rounded-full" style="background-color: {{ $color }};"></i><p></p><div class="collapse md:visible label-bottom">{{ $title }}</div>
     
-        <span class="visible md:invisible">{{ $title }}</span>
+        <span class="block md:collapse">{{ $title }}</span>
 
     </a>
 </li>
