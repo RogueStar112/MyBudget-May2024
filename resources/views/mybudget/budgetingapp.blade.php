e3347748c0c2f24c5b143dde0d0fe2707b53c5a3
RogueStar112 <97138925+RogueStar112@users.noreply.github.com>
2024-05-23 02:59:08 +0100

    budgeting app css remove

--- resources/views/mybudget/budgetingapp.blade.php
+++ resources/views/mybudget/budgetingapp.blade.php
@@ -11,7 +11,6 @@
       <script src="{{ asset('js/app.js') }}" defer></script>
       <link rel="dns-prefetch" href="//fonts.gstatic.com">
       <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
       <link href="{{ asset('css/mylifeline_home.css') }}" rel="stylesheet" type="text/css">
 
