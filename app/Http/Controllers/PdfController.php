7909e6e60b712c91f371d4d3f267212f7956002f
RogueStar112 <hildtgunnt@gmail.com>
2024-06-03 02:21:00 +0100

    Pdf fix attempt 4

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -276,7 +276,7 @@ class PdfController extends Controller
                                     ->select('mybudget_item.*', 'mybudget_item.id as budget_id', 'mybudget_category.name as category_name', 'mybudget_section.name as section_name', 'mybudget_source.name as source_name')
                                     //->selectRaw('PRINTF("%.2f", mybudget_item.price) as price_twodp')
                                     ->selectRaw("mybudget_item.price as price_twodp")
-                                    ->where('section_name', '!=', 'Income')
+                                    // ->where('section_name', '!=', 'Income')
                                     ->whereNull('mybudget_item.deleted_at')
                                     ->whereBetween(\DB::raw('DATE(mybudget_item.created_at)'), [$start_date, $end_date])
                                     ->orderBy('mybudget_item.created_at', 'asc')
